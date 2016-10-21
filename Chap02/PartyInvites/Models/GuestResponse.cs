4e7db5d6ccf2226adad2c4053f6d00a1e0f65030
Charles Utter <Charles Utter>
2016-10-21 15:51:02 -0400

    first

---
+++ Chap02/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "PLease Enter Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please enter valid email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter your phone")]
+        public string phone { get; set; }
+        [Required(ErrorMessage ="Please specify if you will attend")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
